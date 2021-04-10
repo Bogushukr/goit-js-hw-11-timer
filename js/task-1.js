1bb4ef770328ec71ae4f8078f77bcab1ab0d388b
Aleksey Bogush <71962603+Bogushukr@users.noreply.github.com>
2021-04-11 00:48:16 +0300

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,74 +1,49 @@
-class CountdownTimer {
-  #params = ['days', 'hours', 'mins', 'secs'];
-  #handle = null;
-
-  static decorate(value) {
-    return String(value).padStart(2, '0');
-  }
-  static getDays(time) {
-    return this.decorate(Math.floor(time / (1000 * 60 * 60 * 24)));
-  }
-
-  static getHours(time) {
-    return this.decorate(
-      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-    );
-  }
-
-  static getMinutes(time) {
-    return this.decorate(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-  }
-
-  static getSeconds(time) {
-    return this.decorate(Math.floor((time % (1000 * 60)) / 1000));
-  }
-
-  constructor({ selector, targetDate }) {
-    this.targetDate = new Date(targetDate).getTime();
-    this.refs = this.#getReference(selector)
-    this.start();
-  }
-
-  #getReference(selector) {
-    return this.#params.reduce((acc, param) => {
-      acc[param] = document.querySelector(
-        `${selector} [data-value="${param}"]`,
-      );
-      return acc;
-    }, {});
-  }
-
-  static getTime(time) {
-    return {
-      days: this.getDays(time),
-      hours: this.getHours(time),
-      mins: this.getMinutes(time),
-      secs: this.getSeconds(time),
-    };
-  }
-
-
-
-  start() {
-    if (this.#handle) return;
-    this.#handle = setInterval(this.#render.bind(this), 1000);
-  }
-
-  stop() {
-    clearInterval(this.#handle);
-    this.#handle = null;
-  }
-
-  #render() {
-    const time = this.targetDate - Date.now();
-    const data = CountdownTimer.getTime(time);
-    this.#params.forEach(param => {
-      this.refs[param].textContent = data[param];
-    });
-  }
+const refs = {
+  Days: document.querySelector('.value[data-value="days"]'),
+  Hours: document.querySelector('.value[data-value="hours"]'),
+  Mins: document.querySelector('.value[data-value="mins"]'),
+  Secs: document.querySelector('.value[data-value="secs"]')
 }
 
-export default new CountdownTimer({
+const СountdownTimer = function ({ selector, targetDate }) {
+  this.selector = selector;
+  this.targetDate = targetDate;
+
+  this.start = function () {
+    const startTime = targetDate;
+
+    setInterval(() => {
+      const currentTime = Date.now();
+        if (currentTime > startTime) {
+          alert('need to put date in future!!');
+      }
+      const deltaTime = startTime - currentTime;
+      const { days, hours, mins, secs } = getTimer(deltaTime)
+      refs.Days.innerText = days;
+      refs.Hours.innerText = hours;
+      refs.Mins.innerText = mins;
+      refs.Secs.innerText = secs;
+    }, 1000)
+  }
+};
+
+const timer = new СountdownTimer({
   selector: '#timer-1',
   targetDate: new Date('10, 01, 2021'),
 });
+
+timer.start()
+
+
+function pad(value) {
+  return String(value).padStart(2, '0')
+}
+
+function getTimer(time) {
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+  return { days, hours, mins, secs };
+}
